ea318ae3b21feaf0301c5d5b3e0695c5e833bb15
yy3130 <92898782+yy3130@users.noreply.github.com>
2023-03-28 23:18:54 -0400

    changingTemp

--- Morph/Assets/Scripts/TempManager.cs
+++ Morph/Assets/Scripts/TempManager.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using TMPro;
 
 public class TempManager : MonoBehaviour
 {
     public static TempManager instance;
 
-    public Text tempText;
+    
+    public TextMeshProUGUI tempText;
+
+
     int temp = 70;
 
     private void Awake(){
@@ -18,10 +21,16 @@ public class TempManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //tempText = getComponent<TextMeshProUGUI>();
         tempText.text = temp.ToString() + "°C";
     }
 
     // Update is called once per frame
+    private void FixedUpdate() {
+        temp = PlayerData.Pd.temperature;
+        tempText.text = temp.ToString() + "°C";
+
+    }
     public void addTemp(){
         temp ++;
         tempText.text = temp.ToString() + "°C";
