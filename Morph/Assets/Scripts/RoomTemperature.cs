594e523a9c19c747fb617153409876d6f26fc61f
DrPeachy <p1067838263@gmail.com>
2023-04-06 13:59:26 -0400

    the adjustment before yesterday's presentation

--- Morph/Assets/Scripts/RoomTemperature.cs
+++ Morph/Assets/Scripts/RoomTemperature.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RoomTemperature : MonoBehaviour
 {
@@ -42,6 +43,12 @@ public class RoomTemperature : MonoBehaviour
                     PlayerData.Pd.temperature -= multiplier;
                 }
             }
+        }
+
+
+        if(Input.GetKeyDown(KeyCode.R)){
+            string a = SceneManager.GetActiveScene().name;
+            SceneManager.LoadScene(a);
         }
     }
 }
