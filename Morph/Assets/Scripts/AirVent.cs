2285001b603e27b52a58f13d196fe126e5badb19
DrPeachy <p1067838263@gmail.com>
2023-04-10 01:10:12 -0400

    die animation

--- Morph/Assets/Scripts/AirVent.cs
+++ Morph/Assets/Scripts/AirVent.cs
@@ -2,15 +2,70 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
 
 public class AirVent : MonoBehaviour
 {
+
+    private Transform _player;
+
+    private void Awake() {
+        _player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+
     public void OnTriggerEnter2D(Collider2D collision) {
         if (collision.gameObject.name == "Player" && PlayerData.Pd.state == State.Gas) {
             Debug.Log("Gas player hit air vent!");
             // Call function in player to damage
-            string sceneName = SceneManager.GetActiveScene().name;
-            SceneManager.LoadScene(sceneName);
+            StartCoroutine(die());
         }
+    }
+
+
+    private IEnumerator die(){
+        _player.GetComponent<PlayerController>().enabled = false;
+        _player.GetComponent<Rigidbody2D>().isKinematic = true;
+        _player.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        var sr = _player.GetComponent<SpriteRenderer>();
+        sr.color = Color.red;
+        sr
+        .DOFade(
+            0,
+            1f
+        );
+
+        _player
+        .DOLocalJump(
+            Vector3.right * (Random.Range(0, 1) == 1? 1 : -1),
+            1,
+            1,
+            1f
+        )
+        .SetRelative();
+
+        yield return new WaitForSeconds(1.1f);
+
+        _player
+        .DOMove(
+            PlayerData.Pd.lastCheckedPosition,
+            0.1f
+        );
+        PlayerData.Pd.temperature = PlayerData.Pd.lastCheckedTemperature;
+        sr.color = Color.white;
+
+        for (int i = 0; i < 3; i++)
+        {
+            sr.color = new Color(1, 1, 1, 1);
+            yield return new WaitForSeconds(0.2f);
+            sr.color = new Color(1, 1, 1, 0);
+            yield return new WaitForSeconds(0.2f);
+        }
+        sr.color = new Color(1, 1, 1, 1);
+        _player.GetComponent<Rigidbody2D>().isKinematic = false;
+        _player.GetComponent<PlayerController>().enabled = true;
+        _player.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+
+        
     }
 }
--- Morph/Assets/Scripts/Spike.cs
+++ Morph/Assets/Scripts/Spike.cs
@@ -2,15 +2,66 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
 
 public class Spike : MonoBehaviour
 {
+    private Transform _player;
+
+    private void Awake() {
+        _player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
     public void OnCollisionEnter2D(Collision2D collision) {
         if (collision.gameObject.CompareTag("Player") && PlayerData.Pd.state == State.Solid) {
             Debug.Log("Solid player hit spike!");
             // Call function in player to damage
-            string sceneName = SceneManager.GetActiveScene().name;
-            SceneManager.LoadScene(sceneName);
+            StartCoroutine(die());
         }
+    }
+
+    private IEnumerator die(){
+        _player.GetComponent<PlayerController>().enabled = false;
+        _player.GetComponent<Rigidbody2D>().isKinematic = true;
+        _player.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        var sr = _player.GetComponent<SpriteRenderer>();
+        sr.color = Color.red;
+        sr
+        .DOFade(
+            0,
+            1f
+        );
+
+        _player
+        .DOLocalJump(
+            Vector3.right * (Random.Range(0, 1) == 1? 1 : -1),
+            1,
+            1,
+            1f
+        )
+        .SetRelative();
+
+        yield return new WaitForSeconds(1.1f);
+
+        _player
+        .DOMove(
+            PlayerData.Pd.lastCheckedPosition,
+            0.1f
+        );
+        PlayerData.Pd.temperature = PlayerData.Pd.lastCheckedTemperature;
+        sr.color = Color.white;
+
+        for (int i = 0; i < 3; i++)
+        {
+            sr.color = new Color(1, 1, 1, 1);
+            yield return new WaitForSeconds(0.2f);
+            sr.color = new Color(1, 1, 1, 0);
+            yield return new WaitForSeconds(0.2f);
+        }
+        sr.color = new Color(1, 1, 1, 1);
+        _player.GetComponent<Rigidbody2D>().isKinematic = false;
+        _player.GetComponent<PlayerController>().enabled = true;
+        _player.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+
+        
     }
 }
