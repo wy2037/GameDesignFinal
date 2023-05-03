074906165bee821e3d26bf9550782389d1f71a1e
William Yang <wy2037@nyu.edu>
2023-05-03 17:22:48 -0400

    Level adjustments + Glow adjustments

--- Morph/Assets/Scripts/Glow.cs
+++ Morph/Assets/Scripts/Glow.cs
@@ -11,17 +11,18 @@ public class Glow : MonoBehaviour
 {
     public Type objectType;
     public State stateType;
+    [SerializeField] float alphaValue;
     
     void Update() {
-        if (PlayerData.Pd.state == stateType) {
-            if (objectType == Type.Hazard) {
-                gameObject.GetComponent<Renderer>().material.color = Color.red;
-            }
-            if (objectType == Type.Interactable) {
-                gameObject.GetComponent<Renderer>().material.color = Color.green;
-            }
+        if (objectType == Type.Hazard && PlayerData.Pd.state == stateType) {
+            gameObject.GetComponent<Renderer>().material.color = Color.red;
+            gameObject.GetComponent<Renderer>().material.color = new Vector4(1,0,0,1);
+        } else if (objectType == Type.Interactable && PlayerData.Pd.state != stateType) {
+            gameObject.GetComponent<Renderer>().material.color = Color.green;
+            gameObject.GetComponent<Renderer>().material.color = new Vector4(0,1,0,1);
         } else {
             gameObject.GetComponent<Renderer>().material.color = Color.white;
+            gameObject.GetComponent<Renderer>().material.color = new Vector4(1,1,1,alphaValue);
         }
     }
 }
